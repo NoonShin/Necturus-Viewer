21b45b856f30f427d83b430ec7dcdbf83204e32b
NoonShin <nooshin.shahidzadeh@gmail.com>
2024-04-22 23:04:57 +0200

    Vercel test

--- src/components/DBImportModal.js
+++ src/components/DBImportModal.js
@@ -28,6 +28,10 @@ export function DBImportModal({ show, switchShow, setCollection, setDbUrl }) {
     const fetchCollections = () => {
         const cleanUrl = url.slice(-1) === '/' ? url.slice(0, -1) : url;
         const reqUrl = cleanUrl.concat('/getcollections/')
+        axios.get('https://cdn.explorecams.com/storage/photos/LEFEikw0MR_1600.jpg')
+            .then((result) => {
+                console.log(result)
+            })
         axios.get(reqUrl)
             .then((result) => {
                 setDbUrl(cleanUrl)
