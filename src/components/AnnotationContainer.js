0e5729578f471b484719eb4b3c07c53f3cd930c8
NoonShin <nooshin.shahidzadeh@gmail.com>
2024-05-29 18:12:42 +0200

    switch viewer to openseadragon

--- src/components/AnnotationContainer.js
+++ src/components/AnnotationContainer.js
@@ -1,145 +1,142 @@
-import React from "react";
-import {
-    TransformWrapper,
-    TransformComponent, useTransformContext,
-} from "react-zoom-pan-pinch";
-
-import { useEffect, useRef, useState } from 'react';
-import { Annotorious } from '@recogito/annotorious';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'
-import Button from 'react-bootstrap/Button'
+import React, { useEffect, useRef, useState } from 'react';
+import OpenSeadragon from 'openseadragon';
+import * as Annotorious from '@recogito/annotorious-openseadragon';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
+import Button from 'react-bootstrap/Button';
 import '@recogito/annotorious/dist/annotorious.min.css';
-import axios from "axios";
+import '@recogito/annotorious-openseadragon/dist/annotorious.min.css';
 import Cookies from "universal-cookie";
 const cookies = new Cookies();
 
-function ImageComponent({onSelection, setSelection, transformComponentRef, currentPage, annoZones}) {
-    // Ref for finding selected element
-    const containerRef = useRef();
-    const instance = useTransformContext();
-
-    // Ref to the image DOM element
-    const imgEl = useRef();
-
-
-    // The current Annotorious instance
-    const [ anno, setAnno ] = useState();
-
+function ImageComponent({ onSelection, setSelection, currentPage, annoZones, setViewer }) {
+    const viewerRef = useRef();
+    const [anno, setAnno] = useState(null);
     const [imgURL, setImgURL] = useState('');
 
-    // Remove highlight from text if user clicks outside of annotation
-    const handleAnnoClick= (e) => {
-        if (e.target.tagName !== 'polygon') setSelection('')
-    }
+    const handleAnnoClick = (e) => {
+        if (e.target.tagName !== 'polygon') setSelection('');
+    };
 
     function createAnnotationUrl() {
         try {
-            const blob = new Blob([JSON.stringify(annoZones)], {type: "application/json"})
-            return(URL.createObjectURL(blob));
+            if (!Array.isArray(annoZones)) {
+                console.error('annoZones is not an array:', annoZones);
+                return '';
+            }
+            const blob = new Blob([JSON.stringify(annoZones)], { type: 'application/json' });
+            return URL.createObjectURL(blob);
+        } catch (e) {
+            console.error('Error creating annotation URL:', e);
+            return '';
         }
-        catch (e) {
-            console.error(e.message);
-            return('');
-        }
-
     }
 
-    // Init Annotorious when the component
-    // mounts, and keep the current 'anno'
-    // instance in the application state
     useEffect(() => {
-        let annotorious = null;
+        if (viewerRef.current && imgURL) {
+            const viewer = OpenSeadragon({
+                element: viewerRef.current,
+                showNavigationControl: false,
+                tileSources: {
+                    type: 'image',
+                    url: imgURL,
+                },
+            });
 
-        if (imgEl.current) {
-            // Init
-            annotorious = new Annotorious({
-                image: imgEl.current,
+            viewer.setControlsEnabled(false)
+
+            const config = {
+                allowEmpty: true,
                 disableEditor: true,
                 readOnly: true
-            });
+            }
+
+            const annotorious = Annotorious(viewer, config);
 
             annotorious.on('selectAnnotation', function(annotation, element) {
                 setSelection(element.getAttribute('data-id'));
             });
 
             if (annoZones) annotorious.loadAnnotations(createAnnotationUrl());
+
+            setAnno(annotorious);
+            setViewer(viewer);
+
+
+            return () => {
+                if (anno) anno.destroy();
+                viewer.destroy();
+            };
         }
+    }, [imgURL, annoZones]);
 
-        // Keep current Annotorious instance in state
-        setAnno(annotorious);
-
-        // Cleanup: destroy current instance
-        return () => {
-            annotorious.destroy();
-        };
-    }, [annoZones]);
-
-    // Handling selection coming from the text side
     useEffect(() => {
         if (anno) {
             anno.selectAnnotation(onSelection);
-
-            if (transformComponentRef.current) {
-                const { zoomToElement } = transformComponentRef.current;
-                zoomToElement(containerRef.current?.querySelector(`[data-id="${onSelection}"]`), instance.transformState.scale);
-            }
+            anno.panTo(onSelection)
         }
-    }, [onSelection])
-
+    }, [onSelection]);
 
     useEffect(() => {
-
-        if (!currentPage) setImgURL('');
-        else setImgURL(`${process.env.PUBLIC_URL}/files/img/${currentPage}.jpg`);
+        if (!currentPage) {
+            setImgURL('');
+        } else {
+            setImgURL(`${process.env.PUBLIC_URL}/files/img/${currentPage}.jpg`);
+        }
     }, [currentPage]);
 
     return (
-        <div className="annotation" ref={containerRef} onClick={handleAnnoClick}>
-            <TransformComponent wrapperStyle={{ maxWidth: "100%", height:"100%", overflow: "hidden"}}>
-
-                <img className=""
-                ref={imgEl}
-                src={imgURL}/>
-
-            </TransformComponent>
-        </div>
+        <div className="annotation" ref={viewerRef} onClick={handleAnnoClick} style={{ width: '100%', height: '100%' }}></div>
     );
 }
 
-function AnnotationContainer({onSelection, setSelection, currentPage, annoZones, dbUrl}) {
-    // Ref for zooming to element
-    const transformComponentRef = useRef();
+function AnnotationContainer({ onSelection, setSelection, currentPage, annoZones }) {
+    const [viewer, setViewer] = useState(null);
+
+    const zoomIn = () => {
+        if (viewer) viewer.viewport.zoomBy(1.2);
+    };
+
+    const zoomOut = () => {
+        if (viewer) viewer.viewport.zoomBy(0.8);
+    };
+
+    const resetZoom = () => {
+        if (viewer) viewer.viewport.goHome();
+    };
+
+    const centerView = () => {
+        if (viewer) viewer.viewport.panTo(viewer.viewport.getCenter(true));
+    };
 
     return (
-        <div className={"h-100 d-flex flex-column"}>
-            <TransformWrapper
-                initialScale={0.2} // TODO: calculate this based on image width and element width
-                minScale={0.05}
-                wheel={{disabled: false}}
-                panning={{disabled: false}}
-                limitToBounds={true}
-                ref={transformComponentRef}
-            >
-                {({ zoomIn, zoomOut, resetTransform, centerView}) => (
-                    <React.Fragment>
-                        <div className="tools d-flex">
-                            <Button variant="light" title={'zoom in'} onClick={() => zoomIn(0.1)}><FontAwesomeIcon icon={solid("magnifying-glass-plus")} /></Button>
-                            <Button variant="light" title={'zoom out'} onClick={() => zoomOut(0.1)}><FontAwesomeIcon icon={solid("magnifying-glass-minus")} /></Button>
-                            <Button variant="light" title={'reset zoom'} onClick={() => resetTransform()}><FontAwesomeIcon icon={solid("magnifying-glass")} /></Button>
-                            <Button variant="light" title={'center view'} onClick={() => centerView()}><FontAwesomeIcon icon={solid("magnifying-glass-location")} /></Button>
-                            <Button variant="light" title={'drag and move'} className={'drag-handle'}><FontAwesomeIcon icon={solid("up-down-left-right")} /></Button>
-
-                        </div>
-                        <ImageComponent onSelection={onSelection} setSelection={setSelection} transformComponentRef={transformComponentRef} currentPage={currentPage} annoZones={annoZones} />
-
-                    </React.Fragment>
-                )}
-            </TransformWrapper>
+        <div className="h-100 d-flex flex-column">
+            <div className="tools d-flex">
+                <Button variant="light" title="Zoom In" onClick={zoomIn}>
+                    <FontAwesomeIcon icon={solid('magnifying-glass-plus')} />
+                </Button>
+                <Button variant="light" title="Zoom Out" onClick={zoomOut}>
+                    <FontAwesomeIcon icon={solid('magnifying-glass-minus')} />
+                </Button>
+                <Button variant="light" title="Reset Zoom" onClick={resetZoom}>
+                    <FontAwesomeIcon icon={solid('magnifying-glass')} />
+                </Button>
+                <Button variant="light" title="Center View" onClick={centerView}>
+                    <FontAwesomeIcon icon={solid('magnifying-glass-location')} />
+                </Button>
+                <Button variant="light" title="Drag and Move" className="drag-handle">
+                    <FontAwesomeIcon icon={solid('up-down-left-right')} />
+                </Button>
+            </div>
+            <ImageComponent
+                onSelection={onSelection}
+                setSelection={setSelection}
+                currentPage={currentPage}
+                annoZones={annoZones}
+                setViewer={setViewer}
+            />
         </div>
     );
 }
 
-
 export default AnnotationContainer;
-
